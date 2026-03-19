be75a7e6dfdd87d3c384425d271b8447a93fd359
Gustavo <gustavobiggg64@gmail.com>
2026-03-19 10:38:37 -0300

    feat: created knowledge and adjustment header

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,13 @@ const nome = document.getElementById("gustavo");
 
 const textoSobre = document.querySelector(".text");
 const h2Sobre = document.querySelector(".h2-about-me");
+const h2Knowledge = document.querySelector(".h2-knowledge");
 const imagemSobre = document.querySelector(".img-about-me img");
+const cards = document.querySelectorAll(".card-knowledge");
 
 let i = 0;
 let j = 0;
+let nomeJaDigitado = false;
 
 function digitarTitulo() {
     if (i < texto_gustavo_developer.length) {
@@ -21,6 +24,7 @@ function digitarTitulo() {
 
 digitarTitulo();
 
+
 function digitarNome() {
     if (j < texto_gustavo.length) {
         nome.innerHTML += texto_gustavo.charAt(j);
@@ -30,22 +34,29 @@ function digitarNome() {
 }
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
 
         if (entry.isIntersecting) {
 
             entry.target.classList.add("aparecer");
 
-            if (entry.target === nome) {
+            if (entry.target === textoSobre && !nomeJaDigitado) {
                 digitarNome();
+                nomeJaDigitado = true;
             }
 
         }
 
     });
-}, { threshold: 0.9 });
+}, { threshold: 0.6 });
 
-observer.observe(nome);
+
 observer.observe(textoSobre);
 observer.observe(h2Sobre);
 observer.observe(imagemSobre);
+observer.observe(h2Knowledge);
+
+cards.forEach((card, index) => {
+    observer.observe(card);
+    card.style.transitionDelay = `${index * 0.2}s`;
+});
